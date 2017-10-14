40de948876d0461b39cb06a9426c8d84a28b9b97
李铭昕 <715557344@qq.com>
2017-10-14 15:24:02 +0800

    * Fix

---
+++ app/core/Exception/HttpException.php
@@ -0,0 +1,16 @@
+<?php
+// +----------------------------------------------------------------------
+// | HttpException.php [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 limingxinleo All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <https://github.com/limingxinleo>
+// +----------------------------------------------------------------------
+namespace App\Core\Exception;
+
+use Exception;
+
+class HttpException extends Exception
+{
+
+}
