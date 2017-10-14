a11ae256d15b008176dd6fd307b3bb11a67a37de
李铭昕 <715557344@qq.com>
2017-10-14 21:38:37 +0800

    * 增加注释

--- thrift/Github.thrift
+++ thrift/Github.thrift
@@ -3,7 +3,12 @@ namespace php Xin.Thrift.GithubService
 include "github/commits.thrift"
 
 service Github {
+    // 触发消息队列 收到的公共事件订阅信息
     bool receivedEvents(string token)
+
+    // 触发消息队列 查看当前的commits数
     bool commits(string committer, string token)
+    
+    // 显示时间段内，commits变化
     list<commits.CommitsLog> commitsLog(string committer, i32 btime, i32 etime)
 }
