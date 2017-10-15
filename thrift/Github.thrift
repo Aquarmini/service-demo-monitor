ce71bcecbfc4f37243cdd578b1a67bd13a926f3e
李铭昕 <715557344@qq.com>
2017-10-15 09:46:27 +0800

    *

--- thrift/Github.thrift
+++ thrift/Github.thrift
@@ -8,7 +8,7 @@ service Github {
 
     // 触发消息队列 查看当前的commits数
     bool commits(string committer, string token)
-    
+
     // 显示时间段内，commits变化
     list<commits.CommitsLog> commitsLog(string committer, i32 btime, i32 etime)
 }
