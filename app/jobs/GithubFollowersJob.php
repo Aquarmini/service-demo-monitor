6b3f65189d4ac82ead05e23b73917a29a73c748c
李铭昕 <715557344@qq.com>
2017-10-18 11:14:48 +0800

    * Fix Bug

--- app/jobs/GithubFollowersJob.php
+++ app/jobs/GithubFollowersJob.php
@@ -21,14 +21,16 @@ class GithubFollowersJob implements JobInterface
     public function handle()
     {
         $continue = true;
-        
+
         $redis_key = "followers:" . $this->username;
         $page = Redis::get($redis_key) ?? 1;
+        $page = intval($page);
+        if ($page <= 0) $page = 1;
         while ($continue) {
             $res = Follow::followers($this->username, $page, 20, $this->token);
             if (count($res) == 0) {
                 Redis::del($redis_key);
-                $continue = false;
+                break;
             }
             $page++;
             Redis::set($redis_key, $page);
