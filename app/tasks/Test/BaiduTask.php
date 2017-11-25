ccd563647c3e60c7c27a380badd3a045f1b5054b
李铭昕 <715557344@qq.com>
2017-11-25 21:24:34 +0800

    修改为默认

--- app/tasks/Test/BaiduTask.php
+++ app/tasks/Test/BaiduTask.php
@@ -10,28 +10,28 @@ class BaiduTask extends Task
 
     public function tiebaSignAction()
     {
-        $client = BaiduClient::getInstance(['port' => 52100]);
+        $client = BaiduClient::getInstance();
         $res = $client->tiebaSign(env('BAIDU_USS'), '桃园丶龙玉箫');
         dump($res);
     }
 
     public function tiebasAction()
     {
-        $client = BaiduClient::getInstance(['host' => 'my.server.host', 'port' => 52100]);
+        $client = BaiduClient::getInstance();
         $res = $client->myTiebas(env('BAIDU_USS'), '桃园丶龙玉箫');
         dump($res);
     }
 
     public function tiebaAction()
     {
-        $client = BaiduClient::getInstance(['host' => 'my.server.host', 'port' => 52100]);
+        $client = BaiduClient::getInstance();
         $res = $client->tieba(env('BAIDU_USS'), '桃园丶龙玉箫', '上海');
         dump($res);
     }
 
     public function tiebaListAction()
     {
-        $client = BaiduClient::getInstance(['host' => 'my.server.host', 'port' => 52100]);
+        $client = BaiduClient::getInstance();
         $res = $client->tiebaList('桃园丶龙玉箫');
         dump($res);
     }
