d8703fb05ce340e6da1ef1db954a1a29b6a81387
李铭昕 <715557344@qq.com>
2018-01-06 22:38:30 +0800

    增加新产品监控

--- app/tasks/Test/LvTask.php
+++ app/tasks/Test/LvTask.php
@@ -27,7 +27,7 @@ class LvTask extends Task
             ->bindGoods('http://www.louisvuitton.cn/zhs-cn/products/pochette-metis-monogram-006115')
             ->bindGoods('http://www.louisvuitton.cn/zhs-cn/products/dandy-briefcase-pm-epi-014551')
             ->bindGoods('http://www.louisvuitton.cn/zhs-cn/products/pochette-metis-monogram-empreinte-nvprod630173v#M44071')
-            ->bindGoods('https://www.louisvuitton.cn/zhs-cn/products/toiletry-pouch-26-monogram-000767')
+            ->bindGoods('https://www.louisvuitton.cn/zhs-cn/products/toiletry-pouch-26-monogram-000767#M47542')
             ->bindEmails(env('LV_LIMX_NAME'), env('LV_LIMX_EMAIL'))
             ->bindEmails(env('LV_AGNES_NAME'), env('LV_AGNES_EMAIL'))
             ->all();
