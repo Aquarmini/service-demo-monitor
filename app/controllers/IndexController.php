88d21c4a1f4998bba3a0611aecc2f1fa3bc03d51
李铭昕 <715557344@qq.com>
2018-02-05 11:14:33 +0800

    修改异常捕获逻辑

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -8,7 +8,10 @@
 // +----------------------------------------------------------------------
 namespace App\Controllers;
 
+use App\Common\Enums\ErrorCode;
+use App\Common\Exceptions\BizException;
 use App\Core\System;
+use App\Utils\Response;
 
 class IndexController extends Controller
 {
@@ -21,12 +24,17 @@ class IndexController extends Controller
     public function indexAction()
     {
         if ($this->request->isPost()) {
-            return $this->response->setJsonContent([
+            return Response::success([
                 'version' => System::getInstance()->version(),
                 'message' => "You're now flying with Phalcon. Great things are about to happen!",
             ]);
         }
         $this->view->version = System::getInstance()->version();
         return $this->view->render('index', 'index');
+    }
+
+    public function exceptionAction()
+    {
+        throw new BizException(ErrorCode::$ENUM_SYSTEM_ERROR);
     }
 }
